ffe8d3196bb86b25eebc873333b58c59b0a59bbd
antonioRenato <antoniorpfs@gmail.com>
2023-05-31 23:10:58 -0300

    fixed radio to checkbox

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,7 +4,7 @@ import styles from './Task.module.css'
 export function Task () {
   return (
     <div className={styles.container}>
-      <input type="radio" />
+      <input type="checkbox" id="task" name="task" value="content" />
       <label>{` `}Integer urna interdum massa libero auctor neque Integer urna interdum massa libero auctor neque.</label>
       <button className={styles.trash}>
         <Trash size={24} color="var(--gray-300)"/>
