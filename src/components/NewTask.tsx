90782b9eb63cbd1c112025a3ab72120fe8108b6d
antonioRenato <antoniorpfs@gmail.com>
2023-06-04 13:17:16 -0300

    finished to-do list 1.0

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -11,8 +11,14 @@ interface Task {
 export function NewTask() {
   const [tasks, setTasks] = useState<Task[]>([])
   const [newTask, setNewTask] = useState("")
+  const [isValid, setIsValid] = useState(false);
 
   function handleNewTask () {
+    if (newTask.trim() === "" || tasks.some(task => task.content === newTask)) {
+      setIsValid(false);
+      return;
+    }
+
     const newTasks: Task = {
       content: newTask,
       isFinished: false,
@@ -21,17 +27,20 @@ export function NewTask() {
     // Atualize o estado das tarefas adicionando a nova tarefa ao array existente
     setTasks([...tasks, newTasks])
     setNewTask("")
+    setIsValid(true);
   }
 
   function handleNewTaskChanged(event: React.ChangeEvent<HTMLInputElement>) {
     const value = event.target.value
     
     setNewTask(value)
+    setIsValid(/^[a-zA-Z]+$/.test(value));
   }
 
   function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
-    if (event.key === "Enter") {
+    if (event.key === "Enter" && isValid) {
       handleNewTask();
+      setNewTask("");
     }
   }
 
@@ -39,26 +48,64 @@ export function NewTask() {
     setTasks(tasks.filter(task => task.content !== contentToDelete))
   }
 
+  function totalTaskDone(): number {
+    return tasks.filter(task => task.isFinished).length;
+  }
+
+  function handleMarkedDownTask(content: string, isFinished: boolean) {
+    setTasks(tasks.map(task => 
+      task.content === content ? {...task, isFinished } : task  
+    ))
+  }  
+
   return (
     <>
     <div className={styles.newTask}>
-      <input type="text" placeholder="  Adicione uma nova tarefa..." name="content" onChange={handleNewTaskChanged} onKeyDown={handleKeyDown}/>
-      <button type="button" onClick={handleNewTask} >
+        <input 
+          type="text" 
+          placeholder="  Adicione uma nova tarefa..." 
+          name="content" 
+          onChange={handleNewTaskChanged} 
+          onKeyDown={handleKeyDown} 
+          value={newTask}
+        />
+       
+        <button 
+          type="button" 
+          onClick={() => {
+            handleNewTask();
+            setNewTask("");
+          }} 
+          disabled={!isValid}
+        >
         Criar 
         <PlusCircle size={28} />
       </button>
     </div>
 
+    <div>
+        {!isValid && tasks.length > 0 && (
+          <p className={styles.validationMessage}>
+            A tarefa não pode ser repetida nem estar vazia.
+          </p>
+        )}
+    </div>
+
     <div className={styles.container}>
       <span>Tarefas criadas<div>{tasks.length}</div></span>
       
-      <span>Concluídas<div>0</div></span>      
+      <span>Concluídas<div>{totalTaskDone()} de {tasks.length}</div></span>      
     </div>
 
     {tasks.length ? (
         <div>
           {tasks.map(task => (
-            <Task key={task.content} taskObj={{ content: task.content , isFinished: task.isFinished }} onDelete={handleDeleteTask} />
+            <Task 
+              key={task.content} 
+              taskObj={{ content: task.content , isFinished: task.isFinished }} 
+              onDelete={handleDeleteTask} 
+              onToggleFinish={handleMarkedDownTask}
+            />
           ))}
         </div>
     ) : (
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,6 @@
 import { Trash } from "phosphor-react";
 import styles from './Task.module.css'
+import { useRef } from "react";
 
 interface TaskProps {
   taskObj: {
@@ -7,17 +8,35 @@ interface TaskProps {
     isFinished: boolean;
   };
   onDelete: (contentToDelete: string) => void;
+  onToggleFinish: (content: string, isFinished: boolean) => void;
 }
 
 export function Task(props: TaskProps) {
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
   function handleDelete() {
     props.onDelete(props.taskObj.content);
   }
 
+  function handleToggleFinish(event: React.ChangeEvent<HTMLInputElement>) {
+    const isChecked = event.target.checked;
+    props.onToggleFinish(props.taskObj.content, isChecked);
+  }
+
+  function handleLabelClick() {
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = !checkboxRef.current.checked;
+      props.onToggleFinish(
+        props.taskObj.content,
+        checkboxRef.current.checked
+      );
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <input type="checkbox" name={`task-${props.taskObj.content}`} value={props.taskObj.content} />
-      <label>{` `}{props.taskObj.content}</label>
+      <input ref={checkboxRef} type="checkbox" name={`task-${props.taskObj.content}`} value={props.taskObj.content} onChange={handleToggleFinish}/>
+      <label className={styles.contLabel} onClick={handleLabelClick}>{` `}{props.taskObj.content}</label>
       <button className={styles.trash} onClick={handleDelete}>
         <Trash size={24}/>
       </button>
